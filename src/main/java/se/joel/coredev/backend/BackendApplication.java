1881bcd85f7758b297e98266ebcc2e3d5a8e2fbe
Joel <joel.swanelid@gmail.com>
2019-05-21 22:53:02 +0200

    Console prints for current IP-address

--- src/main/java/se/joel/coredev/backend/BackendApplication.java
+++ src/main/java/se/joel/coredev/backend/BackendApplication.java
@@ -3,10 +3,20 @@ package se.joel.coredev.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            System.out.println("IP-address: " + ip.getHostAddress());
+            System.out.println("Copy-paste: http://" + ip.getHostAddress() + ":8080/geofences/");
+        } catch (UnknownHostException e) {
+            System.out.println("Could not obtain IP");
+        }
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
